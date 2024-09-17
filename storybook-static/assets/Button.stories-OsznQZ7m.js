18a2e2c830208b090f4d04d363c4018e186db2f1
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-09-17 16:12:37 +0530

    Refactor storybook static files

---
+++ storybook-static/assets/Button.stories-OsznQZ7m.js
@@ -0,0 +1,13 @@
+import{j as g}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const v=({variant:s,...y})=>{let t="button";return s==="secondary"&&(t+=" secondary"),s==="destructive"&&(t+=" destructive"),g.jsx("button",{className:t,...y})};v.__docgenInfo={description:"",methods:[],displayName:"Button",props:{variant:{required:!0,tsType:{name:"union",raw:"'primary' | 'secondary' | 'destructive'",elements:[{name:"literal",value:"'primary'"},{name:"literal",value:"'secondary'"},{name:"literal",value:"'destructive'"}]},description:""}}};const S={title:"Buttton",component:v,args:{children:"Button",variant:"primary"}},r={args:{variant:"primary"}},e={args:{variant:"secondary"}},a={args:{variant:"destructive"}};var n,o,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    variant: "primary"
+  }
+}`,...(c=(o=r.parameters)==null?void 0:o.docs)==null?void 0:c.source}}};var i,m,d;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary"
+  }
+}`,...(d=(m=e.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var u,p,l;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    variant: "destructive"
+  }
+}`,...(l=(p=a.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};const B=["Primary","Secondary","Destructive"];export{a as Destructive,r as Primary,e as Secondary,B as __namedExportsOrder,S as default};
